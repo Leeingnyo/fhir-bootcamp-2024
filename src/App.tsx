660592dd589716d5dd0eb861e3e46fe17757db91
Lee Inyong <dyd1928@naver.com>
2024-07-10 11:04:37 +0900

    Split files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { FormEvent, useMemo, useState } from 'react';
 import './App.css'
-import { StompClientParams, useStompClient } from './hooks/stomp-client'
+import { ChatView } from './features/chat-view';
 import { ChatContextProvider } from './hooks/chat-context';
 
 function App() {
@@ -13,31 +12,3 @@ function App() {
 }
 
 export default App
-
-const ChatView = () => {
-  const params = useMemo<StompClientParams>(() => ({
-    callback: r => setResponseMessage(r.response),
-  }), []);
-  const stompClientRef = useStompClient(params);
-
-  const [responseMessage, setResponseMessage] = useState('');
-
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-
-    stompClientRef.current?.publish({
-      destination: '/pub/request',
-      body: JSON.stringify({ "query": formData.get('query') })
-    });
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <span>{responseMessage}</span>
-      <br />
-      <input name='query' />
-      <button>전송</button>
-    </form>
-  );
-};
