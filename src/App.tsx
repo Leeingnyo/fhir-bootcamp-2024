913329db3cbfd5ec5bea8fee0b79ea96d59746c5
Lee Inyong <dyd1928@naver.com>
2024-07-10 14:25:51 +0900

    Use stomp client

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
+import { FormEvent, useMemo, useState } from 'react';
 import './App.css'
+import { StompClientParams, useStompClient } from './hooks/stomp-client'
 
 function App() {
+  const params = useMemo<StompClientParams>(() => ({
+    callback: r => setResponseMessage(r.response),
+  }), []);
+  const stompClientRef = useStompClient(params);
+
+  const [responseMessage, setResponseMessage] = useState('');
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+
+    stompClientRef.current?.publish({
+      destination: '/pub/request',
+      body: JSON.stringify({ "query": formData.get('query') })
+    });
+  };
+
   return (
-    <>
-    </>
+    <form onSubmit={handleSubmit}>
+      <span>{responseMessage}</span>
+      <br />
+      <input name='query' />
+      <button>전송</button>
+    </form>
   )
 }
 
